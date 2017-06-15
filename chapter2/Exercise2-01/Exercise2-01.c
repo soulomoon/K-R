fc913b4ee2832083d26d55200cfdafc6899ba8be
soulomoon <fwy996602672@gmail.com>
2017-06-15 15:41:26 +0800

    chapter2-01 find out about size

---
+++ chapter2/Exercise2-01/Exercise2-01.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    char c; 
+    unsigned char uc;
+    short s;
+    unsigned short us;
+    int i;
+    unsigned int ui;
+    long l;
+    unsigned long ul;
+    long long ll;
+    unsigned long long ull;
+    float f;
+
+    printf("length: %d\n", sizeof(c));
+    printf("length: %d\n", sizeof(uc));
+    printf("length: %d\n", sizeof(s));
+    printf("length: %d\n", sizeof(us));
+    printf("length: %d\n", sizeof(i));
+    printf("length: %d\n", sizeof(ui));
+    printf("length: %d\n", sizeof(ll));
+    printf("length: %d\n", sizeof(ull));
+    printf("length: %d\n", sizeof(f));
+}
+
+
+
